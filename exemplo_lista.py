736f5579d5cc9a53bc45161cb987c3b1f1dface6
Gilson Langa <giu.langa@gmail.com>
2025-06-24 22:08:44 -0300

    adicionado exemplo_lista

---
+++ exemplo_lista.py
@@ -0,0 +1,21 @@
+produto: str = "sapato"
+produto_2: str = "camiseta"
+produto_3: str = "videogame"
+
+produtos: list = []
+
+print(produtos)
+
+produtos.append(produto)
+produtos.append(produto_2)
+produtos.append(produto_3)
+
+print(produtos)
+
+produtos.pop()
+
+print(produtos)
+
+produtos.remove(produto)
+
+print(produtos)
