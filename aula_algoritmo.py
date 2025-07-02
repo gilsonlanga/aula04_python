d77cda82f71c0f2deaa88f634100d7f9d2241b91
Gilson Langa <giu.langa@gmail.com>
2025-07-02 20:54:08 -0300

    adicionado aula_algoritmo.py

---
+++ aula_algoritmo.py
@@ -0,0 +1,17 @@
+lista_numeros: list = [] #40,50,60,70,0,-408593,1,50
+
+
+def ordernar_lista_numeros(numeros: list) -> list:
+
+    nova_lista_numeros = numeros.copy()
+
+    for i in range(len(nova_lista_numeros)):
+        for j in range(i+1, len(nova_lista_numeros)):
+            if nova_lista_numeros[i] > nova_lista_numeros[j]:
+                nova_lista_numeros[i], nova_lista_numeros[j] = nova_lista_numeros[j], nova_lista_numeros[i]
+
+    return nova_lista_numeros
+
+nova_lista = ordernar_lista_numeros(lista_numeros)
+
+#print(nova_lista)
